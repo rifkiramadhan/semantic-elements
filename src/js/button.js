f333b18e0a7f133443cf0df75d0f3335375873bb
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2023-04-12 01:15:33 +0700

    Add landing [Semantic Elements]

---
+++ src/js/button.js
@@ -0,0 +1,13 @@
+function myButton() {
+  document.getElementById('button1').innerHTML = 'Visited!';
+  document.getElementById('button2').innerHTML = 'Visited!';
+  document.getElementById('button3').innerHTML = 'Visited!';
+  document.getElementById('button4').innerHTML = 'Visited!';
+  document.getElementById('button5').innerHTML = 'Visited!';
+  document.getElementById('button6').innerHTML = 'Visited!';
+  document.getElementById('button7').innerHTML = 'Visited!';
+  document.getElementById('button8').innerHTML = 'Visited!';
+  document.getElementById('button9').innerHTML = 'Visited!';
+  document.getElementById('button10').innerHTML = 'Visited!';
+  document.getElementById('button11').innerHTML = 'Visited!';
+}
